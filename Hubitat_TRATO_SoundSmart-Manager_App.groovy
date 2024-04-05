eb04af93fee0b0e582ae0cb6cd59f9ece77424b3
hhorigian <4301655+hhorigian@users.noreply.github.com>
2024-04-05 12:49:07 -0300

    Add files via upload

---
+++ Hubitat_TRATO_SoundSmart-Manager_App.groovy
@@ -0,0 +1,91 @@
+/*
+
+Copyright 2024 - VH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+-------------------------------------------
+
+Change history:
+
+1.0.0 - @tomw - Initial release.
+2.0.0 - @hhorigian - Versão para BR. SoundSmart. 
+
+
+NOTE: this structure was copied from @jwetzel492's Combined Presence parent app
+https://github.com/joelwetzel/Hubitat-Combined-Presence
+
+NOTE: this structure was copied from @tomw
+
+*/
+
+definition(
+    name: "SoundSmart - Manager",
+    namespace: "TRATO",
+    author: "VH",
+    description: "",
+    category: "Convenience",
+	iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "")
+
+
+preferences {
+     page name: "mainPage", title: "", install: true, uninstall: true
+}
+
+def installed() {
+    log.info "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.info "Updated with settings: ${settings}"
+    initialize()
+}
+
+def initialize() {
+    log.info "There are ${childApps.size()} child apps installed."
+    childApps.each { child ->
+    	log.info "Child app: ${child.label}"
+    }
+}
+
+def installCheck() {         
+	state.appInstalled = app.getInstallationState()
+	
+	if (state.appInstalled != 'COMPLETE') {
+		section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
+  	}
+  	else {
+    	log.info "Parent Installed OK"
+  	}
+}
+
+def mainPage()
+{
+    dynamicPage(name: "mainPage")
+    {
+        installCheck()
+		
+		if (state.appInstalled == 'COMPLETE')
+        {
+            section("<b>SoundSmart manager instances:</b>")
+            {
+                app(name: "anyOpenApp", appName: "SoundSmart - Manager Instance", namespace: "TRATO", title: "<b>Adicionar um novo grupo SoundSmart</b>", multiple: true)
+			}
+		}
+	}
+}
+
+
---
+++ Hubitat_TRATO_SoundSmart-Player_Driver.groovy
@@ -0,0 +1,673 @@
+/*
+
+Copyright 2024 - VH
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+-------------------------------------------
+
+Change history:
+
+1.0.0 - @tomw - Initial release. Created and developed by tomw. 
+2.0.1 - @hhorigian - versão SoundSmart. Changed name to SoundSmart name for compatibility, added some customizationa and fixes for Brazil. 
+
+
+NOTE: this structure was copied from @tomw
+
+*/
+
+metadata
+{
+    definition(name: "SoundSmart - Player", namespace: "TRATO", author: "VH", importUrl: "")
+    {
+        capability "AudioVolume"
+        capability "Configuration"
+        capability "Initialize"
+        capability "MusicPlayer"
+        capability "Refresh"
+        capability "SpeechSynthesis"
+        capability "Switch"
+        //TODO: capability "AudioNotification"
+        
+        command "executeCommand", ["command"]
+        
+        attribute "commStatus", "string"
+    }
+}
+
+preferences
+{
+    section
+    {
+        input "IP_address", "text", title: "IP address of SoundSmart", required: true
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
+    }
+}
+
+def logDebug(msg) 
+{
+    if (logEnable)
+    {
+        log.debug(msg)
+    }
+}
+
+def installed()
+{
+    logDebug("SoundSmart player installed()")
+    
+    initialize()
+}
+
+def initialize()
+{
+    logDebug("SoundSmart player initialize()")
+    
+    sendEvent(name: "commStatus", value: "unknown")
+    
+    refresh()
+    
+    // set voice prompts behavior based on debugging state
+    setVoicePromptsState()
+}
+
+def updated()
+{
+    logDebug("SoundSmart player updated()")
+    
+    configure()
+}
+
+def configure()
+{
+    logDebug("SoundSmart player configure()")
+    
+    unschedule()
+    state.clear()
+    initialize()
+}
+
+def refresh()
+{
+    refresh(false)
+}
+
+def refreshTracks()
+{
+    refreshTracks(false)
+}
+
+
+def refresh(useCachedValues)
+{
+    logDebug("SoundSmart player refresh()")
+    
+    if(null == IP_address)
+    {
+        return
+    }
+    
+    // unschedule refresh, just in case someone did it directly
+    unschedule()
+    
+    updateStatusEx(useCachedValues)
+    updatePlayerStatus(useCachedValues)
+    
+    updateUriAndDesc(true)
+    
+    // refresh once per minute
+    runIn(5, refresh)
+}
+
+
+
+
+def uninstalled()
+{
+    logDebug("SoundSmart player uninstalled()")
+    
+    unschedule()
+}
+
+def play()
+{
+    logDebug("SoundSmart player play()")
+    resume()    
+}
+    
+def resume()
+{
+    logDebug("SoundSmart player resume()")
+    
+    if(httpGetExec("setPlayerCmd:resume"))
+    {
+        refresh()
+    }
+}
+
+def pause()
+{
+    logDebug("SoundSmart player pause()")
+    
+    if(httpGetExec("setPlayerCmd:pause"))
+    {
+        refresh()
+    }
+}
+
+def stop()
+{
+    logDebug("SoundSmart player stop()")
+    
+    if(httpGetExec("setPlayerCmd:stop"))
+    {
+        refresh()
+    }
+}
+
+def mute()
+{
+    logDebug("SoundSmart player mute()")
+    
+    if(httpGetExec("setPlayerCmd:mute:1"))
+    {
+        refresh()
+    }
+}
+
+def unmute()
+{
+    logDebug("SoundSmart player unmute()")
+    
+    if(httpGetExec("setPlayerCmd:mute:0"))
+    {
+        refresh()
+    }
+}
+
+def setLevel(volumelevel)
+{
+    logDebug("SoundSmart player setLevel()")
+    
+    if(!checkCommStatus())
+    {
+        refresh()
+        return
+    }
+    
+    // bound to [0..100]
+    def intVolumeLevel = (volumelevel >= 0) ? ((volumelevel <=100) ? volumelevel : 100) : 0
+    
+    if(httpGetExec("setPlayerCmd:vol:" + intVolumeLevel))
+    {
+        refresh()
+    }
+    
+    // mute if new volumelevel is 0
+    if(volumelevel <= 0)
+    {
+        mute()
+    }
+    else
+    {
+        unmute()
+    }
+}
+
+def setVolume(volumelevel)
+{
+    logDebug("SoundSmart player setVolume()")
+    
+    setLevel(volumelevel)
+}
+
+def volumeDown()
+{
+    logDebug("SoundSmart player volumeDown()")
+    
+    setLevel(getCurrentVolumeLevel() - 10)
+}
+
+def volumeUp()
+{
+    logDebug("SoundSmart player volumeUp()")
+    
+    setLevel(getCurrentVolumeLevel() + 10)
+}
+
+def nextTrack()
+{
+    logDebug("SoundSmart player nextTrack()")
+    
+    if(httpGetExec("setPlayerCmd:next"))
+    {
+        refresh()
+    }
+}
+
+def previousTrack()
+{
+    logDebug("SoundSmart player previousTrack()")
+    
+    if(httpGetExec("setPlayerCmd:prev"))
+    {
+        refresh()
+    }
+}
+
+def playTrack(trackuri)
+{
+    logDebug("SoundSmart player playTrack(${trackuri})")
+    
+    if(httpGetExec("setPlayerCmd:play:${trackuri}"))
+    {
+        refresh()
+    }
+}
+
+def restoreTrack(trackuri)
+{
+    logDebug("SoundSmart player restoreTrack(${trackuri})")
+    
+    playTrack(trackuri)
+}
+
+def resumeTrack(trackuri)
+{
+    logDebug("SoundSmart player resumeTrack(${trackuri})")
+    
+    playTrack(trackuri)
+}
+
+def setTrack(trackuri)
+{
+    logDebug("SoundSmart player setTrack(${trackuri})")
+    
+    // this one is weird.  mute, play track, restore pause or stop, and finally restore mute/unmute
+    
+    refresh()
+    
+    if(!checkCommStatus())
+    {
+        refresh()
+        return
+    }
+    
+    def muteStatus = getPlayerStatus().mute
+    def playStatus = getPlayerStatus().status.toString()
+    
+    mute()
+    playTrack(trackuri)
+    
+    switch(playStatus)
+    {
+        case "paused":
+            pause()
+            break
+        case "stopped":
+            stop()
+            break
+    }
+    
+    if (!muteStatus.toInteger())
+    {
+        unmute()
+    }
+    
+    refresh()
+}
+
+def playText(text)
+{
+    logDebug("SoundSmart player playText(${text})")
+    
+    playTrack(textToSpeech(text, "Joanna").uri)
+}
+
+def speak(text)
+{
+    logDebug("SoundSmart player speak(${text})")
+    
+    playText(text)
+}
+
+def executeCommand(suffix)
+{
+    logDebug("SoundSmart player executeCommand(${suffix})")
+    
+    return httpGetExec(suffix)
+}
+
+def getBaseURI()
+{
+    return "http://" + IP_address + "/httpapi.asp?command="
+}
+
+def updateStatusEx(useCachedValues)
+{
+    def resp_json
+    
+    if(useCachedValues)
+    {
+        resp_json = getStatusEx()
+    }
+    else
+    {
+        resp_json = parseJson(httpGetExec("getStatusEx"))
+        if(resp_json)
+        {
+            setStatusEx(resp_json)
+            sendEvent(name: "commStatus", value: "good")
+        }
+        else
+        {
+            sendEvent(name: "commStatus", value: "error")
+            return
+        }
+    }
+}
+
+def setStatusEx(statusEx)
+{
+    logDebug("statusEx = ${statusEx}")
+    
+    state.StatusEx = statusEx
+}
+
+def getStatusEx()
+{
+    return state.StatusEx
+}
+
+def updatePlayerStatus(useCachedValues)
+{
+    def resp_json
+    
+    if(useCachedValues)
+    {
+        resp_json = getPlayerStatus()
+    }
+    else
+    {
+        resp_json = parseJson(httpGetExec("getPlayerStatus"))
+        if(resp_json)
+        {
+            setPlayerStatus(resp_json)
+            sendEvent(name: "commStatus", value: "good")
+        }
+        else
+        {
+            sendEvent(name: "commStatus", value: "error")
+            return
+        }
+    }
+    
+    // update attributes
+    sendEvent(name: "level", value: resp_json.vol.toInteger())
+    sendEvent(name: "mute", value: (resp_json.mute.toInteger() ? "muted" : "unmuted"))
+    
+    def tempStatus = ""
+    switch(resp_json.status.toString())
+    {
+        case "stop":
+            tempStatus = "stopped"
+            break
+        case "play":
+            tempStatus = "playing"
+            break
+        case "load":
+            tempStatus = "loading"
+            break
+        case "pause":
+            tempStatus = "paused"
+            break
+    }        
+    sendEvent(name: "status", value: tempStatus)
+    
+    //carga o input que está tocando
+    def tempInput = ""
+    switch(resp_json.mode.toString())
+    {
+        case "31":
+            tempInput = "Spotify"
+            break
+        case "40":
+            tempInput = "Line In"
+            break
+        case "41":
+            tempInput = "Bluetooth"
+            break
+        case "43":
+            tempInput = "Óptico"
+            break
+        case "1":
+            tempInput = "Airplay"
+            break 
+        case "0":
+            tempInput = "Sem Input"
+            break
+        case "11":
+            tempInput = "USB"
+            break  
+        case "49":
+            tempInput = "HDMI"
+            break          
+    }        
+    sendEvent(name: "input", value: tempInput)
+}
+
+def setPlayerStatus(playerStatus)
+{
+    logDebug("playerStatus = ${playerStatus}")
+    
+    state.PlayerStatus = playerStatus
+}
+
+def getPlayerStatus()
+{
+    return state.PlayerStatus
+}
+
+def getCurrentVolumeLevel()
+{
+    return getPlayerStatus().vol.toInteger()
+}
+
+def setVoicePromptsState()
+{
+    if(logEnable)
+    {
+        executeCommand("PromptEnable")
+    }
+    else
+    {
+        executeCommand("PromptDisable")
+    }
+}
+
+def checkCommStatus()
+{
+    switch(device.currentValue("commStatus"))
+    {
+        case "good":
+            logDebug("checkCommStatus() success")
+            return true
+        
+        case "error":
+        case "unknown":
+        default:
+            logDebug("checkCommStatus() failed")
+            return false
+    }
+}
+
+def updateUriAndDesc(useCachedValues)
+{
+    if(!useCachedValues)
+    {
+        refresh()
+    }
+    
+    if(!checkCommStatus())
+    {
+        return
+    }
+    
+    //def tmpTrackData = "uri:${hexToAscii(getPlayerStatus().Title)}"
+    def tmpTrackDesc_back = "${hexToAscii(getPlayerStatus().Title)} | ${hexToAscii(getPlayerStatus().Artist)} from ${hexToAscii(getPlayerStatus().Album)}"
+    def tmpTrackDesc = "${hexToAscii(getPlayerStatus().Title)} | ${hexToAscii(getPlayerStatus().Artist)} "  
+    
+    
+    def tmpTitle = hexToAscii(getPlayerStatus().Title)
+    def tmpArtist = hexToAscii(getPlayerStatus().Artist)
+    
+    //se a fonte é spotify e está tocando mando pegar o album  da música.
+    if ((getPlayerStatus().mode == "31") && (getPlayerStatus().status == "play"))
+    
+    {
+        tmpTrackData = "SPOTIFY"
+        def getcoverURI = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=f72ca3d6b5086f9991adbfb3c183912b&artist=" + tmpArtist + "&track=" + tmpTitle + "&autocorrect=1&format=json"    
+    
+        
+        def coverfile2
+        coverfile2 = httpPOSTExec(getcoverURI)
+        def tmpTrackDesc_temp = " ${hexToAscii(getPlayerStatus().Title)} | ${hexToAscii(getPlayerStatus().Artist)} "  
+        def imgfile = "<img src=" + state.AlbumCover + ">"
+        tmpTrackDesc =  imgfile + tmpTrackDesc_temp
+        sendEvent(name: "trackData", value: tmpTrackData)
+        sendEvent(name: "trackDescription", value: tmpTrackDesc)
+        
+    } else
+    {
+        tmpTrackData = "N/A"
+    
+    }
+    
+    
+}
+
+
+def updateIP(ip)
+{
+    if(null == ip)
+    {
+        device.clearSetting("IP_address")
+        return
+    }
+    
+    device.updateSetting("IP_address", ip.toString())
+}
+
+
+def getGroupingDetails()
+{
+    updateStatusEx(false)
+    def statusEx = getStatusEx()
+    
+    if(null == statusEx)
+    {
+        logDebug("failed getGroupingDetails().  check connection and IP")
+        return
+    }
+    
+    // grouping details:
+    // upnp_uuid: UUID from player
+    // ssid: SSID from player
+    // eth2: ethernet IP (0.0.0.0 if not connected)
+    // apcli0: wireless IP
+    // strategy: grouping strategy
+    // groupIP: device IP once grouped (TBD)
+    // name: player name
+    // chan: WifiChannel 
+    
+    def grpStrat = ("0.0.0.0" == statusEx?.eth2) ? "direct" : "router"
+    
+    return([upnp_uuid: statusEx.upnp_uuid, ssid: statusEx.ssid, eth2: statusEx.eth2, apcli0: statusEx.apcli0, strategy: grpStrat,  groupIP: "", name: statusEx.DeviceName, chan: statusEx.WifiChannel])
+}
+
+def hexToAscii(hexStr)
+{
+    return new String(hubitat.helper.HexUtils.hexStringToByteArray(hexStr))
+}
+
+def parseJson(resp)
+{
+    def jsonSlurper = new groovy.json.JsonSlurper()
+    
+    try
+    {
+        resp_json = jsonSlurper.parseText(resp.toString())        
+        return resp_json
+    }
+    catch (Exception e)
+    {
+        log.warn "parse failed: ${e.message}"
+        return null
+    }
+}
+     
+
+def httpGetExec(suffix)
+{
+    logDebug("httpGetExec(${suffix})")
+    
+    try
+    {
+        getString = getBaseURI() + suffix
+        httpGet(getString.replaceAll(' ', '%20'))
+        { resp ->
+            if (resp.data)
+            {
+                logDebug("resp.data = ${resp.data}")
+                return resp.data
+            }
+        }
+    }
+    catch (Exception e)
+    {
+        logDebug("httpGetExec() failed: ${e.message}")
+    }
+}
+
+
+def httpPOSTExec(URI)
+{
+    
+    try
+    {
+        getString = URI
+        segundo = ""
+        httpPostJson(getString.replaceAll(' ', '%20'),segundo,  )
+        { resp ->
+            if (resp.data)
+            {
+                       
+                        def resp_json
+                        def coverfile
+                        resp_json = resp.data
+                        coverfile = resp_json.track.album.image[1]."#text"
+                        //log.info "CoverAlbum Filename " + coverfile 
+                        state.AlbumCover = coverfile
+                                  
+            }
+        }
+    }
+                            
+
+    catch (Exception e)
+    {
+        logDebug("httpPostExec() failed: ${e.message}")
+    }
+    
+}
+
